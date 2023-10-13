8c5765befb4682ad73bab60b8966a9b99f6c66c4
tenksom <joachim@stramke.com>
2023-10-13 22:44:06 +0200

    added name field for name editing

--- src/main/java/guestbook/GuestbookController.java
+++ src/main/java/guestbook/GuestbookController.java
@@ -39,149 +39,163 @@ import org.springframework.web.server.ResponseStatusException;
 @Controller
 class GuestbookController {
 
-	private final GuestbookRepository guestbook;
+    private final GuestbookRepository guestbook;
 
-	/**
-	 * Creates a new {@link GuestbookController} using the given {@link GuestbookRepository}. Spring will look for a bean
-	 * of type {@link GuestbookRepository} and hand this into this class when an instance is created.
-	 *
-	 * @param guestbook must not be {@literal null}
-	 */
-	GuestbookController(GuestbookRepository guestbook) {
+    /**
+     * Creates a new {@link GuestbookController} using the given {@link GuestbookRepository}. Spring will look for a bean
+     * of type {@link GuestbookRepository} and hand this into this class when an instance is created.
+     *
+     * @param guestbook must not be {@literal null}
+     */
+    GuestbookController(GuestbookRepository guestbook) {
 
-		Assert.notNull(guestbook, "Guestbook must not be null!");
-		this.guestbook = guestbook;
-	}
+        Assert.notNull(guestbook, "Guestbook must not be null!");
+        this.guestbook = guestbook;
+    }
 
-	/**
-	 * Handles requests to the application root URI. Note, that you can use {@code redirect:} as prefix to trigger a
-	 * browser redirect instead of simply rendering a view.
-	 *
-	 * @return a redirect string
-	 */
-	@GetMapping(path = "/")
-	String index() {
-		return "redirect:/guestbook";
-	}
+    /**
+     * Handles requests to the application root URI. Note, that you can use {@code redirect:} as prefix to trigger a
+     * browser redirect instead of simply rendering a view.
+     *
+     * @return a redirect string
+     */
+    @GetMapping(path = "/")
+    String index() {
+        return "redirect:/guestbook";
+    }
 
-	/**
-	 * Handles requests to access the guestbook. Obtains all currently available {@link GuestbookEntry}s and puts them
-	 * into the {@link Model} that's used to render the view.
-	 *
-	 * @param model the model that's used to render the view
-	 * @param form the form to be added to the model
-	 * @return a view name
-	 */
-	@GetMapping(path = "/guestbook")
-	String guestBook(Model model, @ModelAttribute(binding = false) GuestbookForm form) {
+    /**
+     * Handles requests to access the guestbook. Obtains all currently available {@link GuestbookEntry}s and puts them
+     * into the {@link Model} that's used to render the view.
+     *
+     * @param model the model that's used to render the view
+     * @param form  the form to be added to the model
+     * @return a view name
+     */
+    @GetMapping(path = "/guestbook")
+    String guestBook(Model model, @ModelAttribute(binding = false) GuestbookForm form) {
 
-		model.addAttribute("entries", guestbook.findAll());
-		model.addAttribute("form", form);
+        model.addAttribute("entries", guestbook.findAll());
+        model.addAttribute("form", form);
 
-		return "guestbook";
-	}
+        return "guestbook";
+    }
 
-	/**
-	 * Handles requests to create a new {@link GuestbookEntry}. Spring MVC automatically validates and binds the HTML form
-	 * to the {@code form} parameter. Validation or binding errors, if any, are exposed via the {@code
-	 * errors} parameter.
-	 *
-	 * @param form the form submitted by the user
-	 * @param errors an object that stores any form validation or data binding errors
-	 * @param model the model that's used to render the view
-	 * @return a redirect string
-	 */
-	@PostMapping(path = "/guestbook")
-	String addEntry(@Valid @ModelAttribute("form") GuestbookForm form, Errors errors, Model model) {
+    /**
+     * Handles requests to create a new {@link GuestbookEntry}. Spring MVC automatically validates and binds the HTML form
+     * to the {@code form} parameter. Validation or binding errors, if any, are exposed via the {@code
+     * errors} parameter.
+     *
+     * @param form   the form submitted by the user
+     * @param errors an object that stores any form validation or data binding errors
+     * @param model  the model that's used to render the view
+     * @return a redirect string
+     */
+    @PostMapping(path = "/guestbook")
+    String addEntry(@Valid @ModelAttribute("form") GuestbookForm form, Errors errors, Model model) {
+        System.out.println("add in there luis");
+        if (errors.hasErrors()) {
+            return guestBook(model, form);
+        }
 
-		if (errors.hasErrors()) {
-			return guestBook(model, form);
-		}
+        guestbook.save(form.toNewEntry());
 
-		guestbook.save(form.toNewEntry());
+        return "redirect:/guestbook";
+    }
 
-		return "redirect:/guestbook";
-	}
-	@PreAuthorize("hasRole('ADMIN')")
-	@PostMapping(path="/guestbook/{entry}")
-	String editEntry(@Valid @ModelAttribute("form") GuestbookForm form, GuestbookEntry entry,  Errors errors, Model model){
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping(path = "/guestbook/{entry}")
+    String editEntry(@Valid @ModelAttribute("form") GuestbookForm form, GuestbookEntry entry, Errors errors, Model model) {
+        System.out.println("get in there luis");
+        entry.setName(form.getName());
+        entry.setText(form.getText());
 
-		entry.setName(form.getName());
-		entry.setText(form.getText());
+        guestbook.save(entry);
+        return "redirect:/guestbook";
+    }
 
-		guestbook.save(entry);
-		return "redirect:/guestbook";
-	}
+    /**
+     * Deletes a {@link GuestbookEntry}. This request can only be performed by authenticated users with admin privileges.
+     * Also note how the path variable used in the {@link DeleteMapping} annotation is bound to an {@link Optional}
+     * parameter of the controller method using the {@link PathVariable} annotation. If the entry couldn't be found, that
+     * {@link Optional} will be empty.
+     *
+     * @param entry an {@link Optional} with the {@link GuestbookEntry} to delete
+     * @return a redirect string
+     */
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping(path = "/guestbook/{entry}")
+    String removeEntry(@PathVariable Optional<GuestbookEntry> entry) {
 
-	/**
-	 * Deletes a {@link GuestbookEntry}. This request can only be performed by authenticated users with admin privileges.
-	 * Also note how the path variable used in the {@link DeleteMapping} annotation is bound to an {@link Optional}
-	 * parameter of the controller method using the {@link PathVariable} annotation. If the entry couldn't be found, that
-	 * {@link Optional} will be empty.
-	 *
-	 * @param entry an {@link Optional} with the {@link GuestbookEntry} to delete
-	 * @return a redirect string
-	 */
-	@PreAuthorize("hasRole('ADMIN')")
-	@DeleteMapping(path = "/guestbook/{entry}")
-	String removeEntry(@PathVariable Optional<GuestbookEntry> entry) {
+        return entry.map(it -> {
 
-		return entry.map(it -> {
+            guestbook.delete(it);
+            return "redirect:/guestbook";
 
-			guestbook.delete(it);
-			return "redirect:/guestbook";
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 
-		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-	}
+    // Request methods answering HTMX requests
 
-	// Request methods answering HTMX requests
+    /**
+     * Handles AJAX requests to create a new {@link GuestbookEntry}. Instead of rendering a complete page, this view only
+     * renders and returns the HTML fragment representing the newly created entry.
+     * <p>
+     * Note that we do not react explicitly to a validation error: in such a case, Spring automatically returns an
+     * appropriate JSON document describing the error.
+     *
+     * @param form  the form submitted by the user
+     * @param model the model that's used to render the view
+     * @return a reference to a Thymeleaf template fragment
+     * @see #addEntry(String, String)
+     */
+    @HxRequest
+    @PostMapping(path = "/guestbook")
+    HtmxResponse addEntry(@Valid GuestbookForm form, Model model) {
+        model.addAttribute("entry", guestbook.save(form.toNewEntry()));
+        model.addAttribute("index", guestbook.count());
 
-	/**
-	 * Handles AJAX requests to create a new {@link GuestbookEntry}. Instead of rendering a complete page, this view only
-	 * renders and returns the HTML fragment representing the newly created entry.
-	 * <p>
-	 * Note that we do not react explicitly to a validation error: in such a case, Spring automatically returns an
-	 * appropriate JSON document describing the error.
-	 *
-	 * @param form the form submitted by the user
-	 * @param model the model that's used to render the view
-	 * @return a reference to a Thymeleaf template fragment
-	 * @see #addEntry(String, String)
-	 */
-	@HxRequest
-	@PostMapping(path = "/guestbook")
-	HtmxResponse addEntry(@Valid GuestbookForm form, Model model) {
+        return new HtmxResponse()
+                .addTemplate("guestbook :: entry")
+                .addTrigger("eventAdded");
+    }
 
-		model.addAttribute("entry", guestbook.save(form.toNewEntry()));
-		model.addAttribute("index", guestbook.count());
+    @HxRequest
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping(path = "/guestbook/{entry}")
+    HtmxResponse editEntryHtmx(@Valid @ModelAttribute("form") GuestbookForm form, GuestbookEntry entry, Errors errors, Model model) {
+        System.out.println("get in there luis");
+        entry.setName(form.getName());
+        entry.setText(form.getText());
 
-		return new HtmxResponse()
-				.addTemplate("guestbook :: entry")
-				.addTrigger("eventAdded");
-	}
+        model.addAttribute(guestbook.save(entry));
 
-	/**
-	 * Handles AJAX requests to delete {@link GuestbookEntry}s. Otherwise, this method is similar to
-	 * {@link #removeEntry(Optional)}.
-	 *
-	 * @param entry an {@link Optional} with the {@link GuestbookEntry} to delete
-	 * @return a response entity indicating success or failure of the removal
-	 * @throws ResponseStatusException
-	 */
-	@HxRequest
-	@PreAuthorize("hasRole('ADMIN')")
-	@DeleteMapping(path = "/guestbook/{entry}")
-	HtmxResponse removeEntryHtmx(@PathVariable Optional<GuestbookEntry> entry, Model model) {
+        return new HtmxResponse().addTemplate("guestbook :: entry").addTrigger("eventEdit");
 
-		return entry.map(it -> {
+    }
 
-			guestbook.delete(it);
+    /**
+     * Handles AJAX requests to delete {@link GuestbookEntry}s. Otherwise, this method is similar to
+     * {@link #removeEntry(Optional)}.
+     *
+     * @param entry an {@link Optional} with the {@link GuestbookEntry} to delete
+     * @return a response entity indicating success or failure of the removal
+     * @throws ResponseStatusException
+     */
+    @HxRequest
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping(path = "/guestbook/{entry}")
+    HtmxResponse removeEntryHtmx(@PathVariable Optional<GuestbookEntry> entry, Model model) {
 
-			model.addAttribute("entries", guestbook.findAll());
+        return entry.map(it -> {
 
-			return new HtmxResponse()
-					.addTemplate("guestbook :: entries");
+            guestbook.delete(it);
 
-		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-	}
+            model.addAttribute("entries", guestbook.findAll());
+
+            return new HtmxResponse()
+                    .addTemplate("guestbook :: entries");
+
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
